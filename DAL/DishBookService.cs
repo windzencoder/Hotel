b3ffe433347929a00ade999f298ac3f4ad672e67
PC-201705191109\Administrator <2099260054@qq.com>
2017-07-19 17:07:36 +0800

    编写DAL层通用SQLHelper

---
+++ DAL/DishBookService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAL
+{
+    public class DishBookService
+    {
+
+    }
+}
---
+++ DAL/DishService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    /// <summary>
+    /// 菜品数据访问类
+    /// </summary>
+    public class DishService
+    {
+
+    }
+}
